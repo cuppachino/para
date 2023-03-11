ecf7c1d2f6331634d81791c44776c676d3c4112b
cuppachino <cuppachino.dev@gmail.com>
2023-03-10 19:35:30 -0600

    add tests parser tests

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use camino::Utf8PathBuf;
+use camino::{Utf8Path, Utf8PathBuf};
 use json_comments::StripComments;
 use rayon::{iter::Either, prelude::*};
 use serde::{Deserialize, Serialize};
@@ -54,7 +54,7 @@ pub fn load_configs(paths: &[Utf8PathBuf]) -> (Vec<ParaConfig>, Vec<&Utf8PathBuf
             Ok(config) => Either::Left(config),
             _ => Either::Right(path),
         }
-        })
+    })
 }
 
 /// This will append a file name to a path if the path is a directory.
@@ -65,4 +65,61 @@ fn normalize_dir_paths(path: &Utf8PathBuf, file: impl AsRef<Utf8Path>) -> Utf8Pa
         path.push(file);
     }
     path
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::utils::Cwd;
+
+    #[test]
+    fn test_normalize_dir_paths() {
+        let cwd = Cwd::new();
+        let a = cwd.clone().join("tsconfig.json");
+
+        assert_eq!(a, normalize_dir_paths(&cwd, "tsconfig.json"));
+        assert_eq!(a, normalize_dir_paths(&a, cwd.join("tsconfig.json")));
+    }
+
+    #[test]
+    fn test_load_configs() {
+        let cwd = Cwd::new();
+        let a = cwd.clone().join("a_tsconfig.json");
+        let b = cwd.clone().join("b_tsconfig.json"); // is missing "outDir"
+        let c = cwd.clone().join("myapp/tsconfig.json");
+
+        let binding = [a, b, c];
+        let (configs, errors) = load_configs(&binding);
+        assert_eq!(configs.len(), 2);
+        assert_eq!(errors.len(), 1);
+        assert_eq!(errors[0], &binding[1])
+    }
+
+    #[test]
+    fn test_parse_tsconfig() {
+        let cwd = Cwd::new();
+        let a = cwd.clone().join("a_tsconfig.json");
+        let b = cwd.clone().join("b_tsconfig.json"); // is missing "outDir"
+        let c = cwd.clone().join("myapp/tsconfig.json");
+
+        let a_config = parse_tsconfig(&a).unwrap();
+        let b_config = parse_tsconfig(&b).ok();
+        let c_config = parse_tsconfig(&c).unwrap();
+
+        assert_eq!(a_config.path, a);
+        assert!(b_config.is_none());
+        assert_eq!(c_config.path, c);
+
+        assert_eq!(a_config.tsconfig.compiler_options.out_dir, "dist");
+        assert_eq!(a_config.tsconfig.compiler_options.base_url, ".");
+        assert_eq!(a_config.tsconfig.compiler_options.paths.len(), 2);
+        assert_eq!(
+            a_config.tsconfig.compiler_options.paths["@/*"],
+            vec!["pkg/*"]
+        );
+        assert_eq!(
+            a_config.tsconfig.compiler_options.paths["$/*"],
+            vec!["node_modules/*"]
+        );
+    }
 }
