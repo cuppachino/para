9e5f962c32116050b38fded5b0d1c04537a798a1
cuppachino <cuppachino.dev@gmail.com>
2023-03-15 04:12:46 -0500

    cleanup imports

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use crate::{
+    args::{handle_cli_cache_command, parse_cli_exclude_paths, parse_cli_paths},
+    cache::{load_cache, save_cache},
+    exclusions::IntoGlobSet,
+    parser::load_configs,
+};
 use clap::Parser;
-use exclusions::*;
+
 use lazy_static::lazy_static;
 use log::{debug, info, verbose, warn, Logger};
 #[allow(unused_imports)]
 use rayon::prelude::*;
 use regex::Regex;
-
-use crate::cache::{load_cache, save_cache};
 
 mod args;
 mod cache;
@@ -22,29 +26,21 @@ fn main() -> std::io::Result<()> {
     let logger = Logger(cli.log_level);
     let cache_dir = cache::generate_cache_dir(&logger)?;
 
-    // Parse - cache command
-    // if let Some(command) = cli.cache {
-    // match command {
-    //     Commands::Cache { clear, dump, get } => {}
-    // }
-    // }
-
     // Exit - if the cache command was used.
-    if let Some(_) = args::handle_cli_cache_command(cli.cache_action, &cache_dir, &logger) {
+    if let Some(_) = handle_cli_cache_command(cli.cache_action, &cache_dir, &logger) {
         return Ok(());
     }
 
     // Parse - tsconfig paths
-    let paths = args::parse_cli_paths(cli.paths, cli.paths_arg, &logger);
+    let paths = parse_cli_paths(cli.paths, cli.paths_arg, &logger);
 
     // Parse - exclude paths
-    let exclude_globset =
-        args::parse_cli_exclude_paths(cli.exclude, cli.merge_with_default_exclude)
-            .into_globset()
-            .unwrap();
+    let exclude_globset = parse_cli_exclude_paths(cli.exclude, cli.merge_with_default_exclude)
+        .into_globset()
+        .unwrap();
 
     // Parse - tsconfig contents
-    let (configs, skipped) = parser::load_configs(&paths);
+    let (configs, skipped) = load_configs(&paths);
     verbose::tsconfigs(&configs, &logger);
     warn::paths_skipped(skipped, &logger);
     info::configs_loaded(paths.len(), configs.len(), &logger);
